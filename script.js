d250be985e5afbf158e1f4c99589ace93abd0a2a
elyonjohn <elyonjohn60@gmail.com>
2024-02-17 22:27:31 +0100

    Updated and Modified Scrip file

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
 alert ("WELCOME TO YOUR MONTHLY CALENDER");
 alert("ENTER THE MONTH NUMBER");
 let month = parseInt (prompt("PLEASE ENTER THE MONTH NUMBER: "));
+alert("PROCESSING..............");
+alert("DONE!");
 switch  (month) {
     case 1:
         alert("JANUARY HAS 31 DAYS");
+        alert("THANK YOU PRESS OK TO EXIT.");
         break;
         case 2:
             alert("FEBRUARY HAS 28 DAYS");
+                    alert("THANK YOU PRESS OK TO EXIT.");
             break;
             case 3:
                 alert("MARCH HAS 31 DAYS");
+                        alert("THANK YOU PRESS OK TO EXIT.");
                 break;
                 case 4:
                     alert("APRIL HAS 30 DAYS");
+                            alert("THANK YOU PRESS OK TO EXIT.");
                     break;
                     case 5:
                         alert("MAY HAS 31 DAYS");
+                                alert("THANK YOU PRESS OK TO EXIT.");
                         break;
                         case 6:
                             alert("JUNE HAS 30 DAYS");
+                                    alert("THANK YOU PRESS OK TO EXIT.");
                             break;
                             case 7:
                                 alert("JULY HAS 31 DAYS");
+                                        alert("THANK YOU PRESS OK TO EXIT.");
                                 break;
                                 case 8:
                                     alert("AUGUST HAS 30 DAYS");
+                                            alert("THANK YOU PRESS OK TO EXIT.");
                                     break;
                                     case 9:
                                         alert("SEPTEMBER HAS 30 DAYS");
+                                                alert("THANK YOU PRESS OK TO EXIT.");
                                         break;
                                         case 10: 
                                         alert("OCTOBER HAS 31 DAYS");
+                                                alert("THANK YOU PRESS OK TO EXIT.");
                                         break;
                                         case 11: 
                                         alert("NOVEMBER HAS 30 DAYS");
+                                                alert("THANK YOU PRESS OK TO EXIT.");
                                         break;
                                         case 12: 
                                         alert("DECEMBER HAS 31 DAYS");
+                                                alert("THANK YOU PRESS OK TO EXIT.");
                                         break;
             default: 
-            console.log("INVALID MONTH NUMBER ");
+            alert("INVALID MONTH NUMBER PLEASE TRY AGAIN");
  }
